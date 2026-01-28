d8fc73aab068a4ccad2665c03c510fcb99b3ddc8
hariasharii94-collab <hariasharii94@gmail.com>
2026-01-29 05:36:24 +0700

    init discord bot

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const { Client, GatewayIntentBits } = require('discord.js')
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+client.once('ready', () => {
+  console.log(`Bot online: ${client.user.tag}`)
+})
+
+client.login(process.env.DISCORD_TOKEN)
+
